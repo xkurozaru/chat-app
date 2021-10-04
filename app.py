eaabd7cde38f11df19dcb2912baf80620c374734
xkurozaru <kurozaru.210@gmail.com>
2021-10-04 20:40:54 +0900

    complete

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
+from enum import unique
 from flask import *
 import sqlite3
 from flask_sqlalchemy import *
 import datetime
+import random
 
 app = Flask(__name__)
 app.secret_key = 'qwertyuiopasdfghjklzxcvbnm'
@@ -11,14 +13,14 @@ db = SQLAlchemy(app)
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String)
-    password = db.Column(db.String)
+    username = db.Column(db.String(20), unique=True)
+    password = db.Column(db.String(20))
 
 class Chat(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String)
-    date = db.Column(db.String)
-    message = db.Column(db.String)
+    username = db.Column(db.String(20))
+    date = db.Column(db.String(20))
+    message = db.Column(db.String(400))
 
 @app.route("/")
 def tologin():
@@ -30,9 +32,10 @@ def login():
 
 @app.route("/logincheck", methods=["POST"])
 def logincheck():
-    username = request.form.get("username")
-    password = request.form.get("password")
-    id = User.query.order_by().filter(User.username==username).filter(User.password==password).first()
+    username = request.form.get('username')
+    password = request.form.get('password')
+    id = User.query.filter(username==username).filter(password==password).first().id
+
     print(id)
     if id is None:
         return redirect("/login")
@@ -42,28 +45,32 @@ def logincheck():
 
 @app.route("/regist", methods=["POST"])
 def regist():
-    username = request.form.get("username")
-    password = request.form.get("password")
-    entry = User()
-    entry.username = username
-    entry.password = password
-    db.session.add(entry)
-    db.session.commit
+    username = request.form.get('username')
+    password = request.form.get('password')
+    id = random.randint(1,99999999)
+    print(username)
+    print(password)
+    print(id)
+
+    new_user = User(id=id,username=username,password=password)
+    db.session.add(new_user)
+    db.session.commit()
+
     return redirect("/login")
 
 @app.route("/mypage")
 def mypage():
     id = session['user_id']
-    username = User.query.order_by().filter(User.id==id).first()
+    username = User.query.filter(User.id==id).first().username
     chats = Chat.query.order_by(Chat.date.desc()).filter(Chat.username==username).all()
     return render_template("mypage.html", username=username, chats=chats)
     
 @app.route("/send", methods=["POST"])
 def send():
+    id = session['user_id']
     date = str(datetime.datetime.now().replace(microsecond = 0))
     message = request.form.get('message')
-    id = session['user_id']
-    username = User.query.order_by().filter(User.id==id).first()
+    username = User.query.filter(User.id==id).first().username
     new_chat = Chat(username=username,date=date,message=message)
     db.session.add(new_chat)
     db.session.commit()
